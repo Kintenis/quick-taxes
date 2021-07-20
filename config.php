8bc22ccadd3c3c07c64d16779cdeb4370de31e51
kintenis <knitenis@gmail.com>
2021-07-20 20:40:19 +0300

    Connection with a database & SELECT query

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$serverName = "localhost";
+$username = "root";
+$password = "admin";
+$dbName = "quick_taxes";
+
+$db = new mysqli($serverName, $username, $password, $dbName);
--- taxes.php
+++ taxes.php
@@ -1,5 +1,12 @@
 <?php
 
+include_once 'config.php';
+
+if ($db->connect_errno) {
+    echo "Failed to connect to MySQL: " . $db->connect_error;
+    exit();
+}
+
 echo "Metai: " . $_GET["year"] . "<br>";
 echo "Mėnesis: " . $_GET["month"] . "<br>";
 echo "Karštas WC: " . $_GET["hot-wc"] . "<br>";
@@ -7,3 +14,10 @@ echo "Šaltas WC: " . $_GET["cold-wc"] . "<br>";
 echo "Karštas Virt.: " . $_GET["hot-kitchen"] . "<br>";
 echo "Šaltas Virt.: " . $_GET["cold-kitchen"] . "<br>";
 echo "Elektra: " . $_GET["electricity"] . "<br>";
+
+$sql = "SELECT * FROM counter_data ORDER BY f_id DESC LIMIT 1";
+$query = mysqli_query($db, $sql);
+$data = mysqli_fetch_all($query);
+
+echo '<pre>';
+var_dump($data);
